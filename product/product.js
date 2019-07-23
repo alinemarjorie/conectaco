c3dfdb1064833ea24d3be04956ef20c27f370ea2
PriTheodoro <pri.paf@gmail.com>
2019-07-23 19:03:37 -0300

    fluxo da pagina de produtos

---
+++ product/product.js
@@ -0,0 +1,23 @@
+const infoPay = {
+    product: "",
+    platform_id: "",
+    provider_id: "",
+    donation_id: "",
+    value_platform: "",
+    value_provider: "",
+    value_donation:"",
+    value_total:"",
+}
+
+const insertInfoPayInKart = () => {
+    let product = "product";
+    let platform_id = "";
+    let provider_id = "";
+    let donation_id = "";
+    let value_platform = "platform-value";
+    let value_provider = "provider-value";
+    let value_donation = "donation-value";
+    let value_total = "";
+
+}
+
