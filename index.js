52b67c61dfd4e2efcf131a0e0c1028cea050d2ba
Jéssica Happatsch <vonhappatsch@gmail.com>
2019-07-24 16:51:50 -0300

    final

--- index.js
+++ index.js
@@ -9,11 +9,12 @@ const allProducts = (items) => {
       ${items.map(item => `
           <div class="card-deck">
             <div class="card mb-5 mr-5">
-              <img class="card-img-top img-responsive" src="${item["image"]}" alt="product image">
+              <img class="card-img-top img-responsive" id="goBuy" src="${item["image"]}" alt="product image">
               <div class="card-body">
                   <h5 class="card-title product-title">${item["title"]}</h5>
                   <p class="card-text artist">${item["artist"]}</p>
-                  <p class="card-text price">Valor mínimo: R$${item["min_price"]}</p>
+                  <p class="card-text price">Valor mínimo: R$ ${item["min_price"]}</p>
+                  <a href="./product/product.html" class="badge badge-light">Ver mais</a>
               </div>
             </div>
           </div>
